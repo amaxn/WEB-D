93bbc7a35535ba28bc781f4003107488d453cf63
Aman <apoddar223@gmail.com>
2020-07-27 00:55:01 +0530

    deleted files

--- WEB D/java_km.js
+++
@@ -1,5 +0,0 @@
-var km = prompt("Enter the ditance in kilometers, below");
-var m;
-m = km*1000;
-alert("Distance in meters is" + " " + m + " " +"m");
-console.log("Distance in meters is" + " " + m + " " + "m");
--- WEB D/jscript.js
+++
@@ -1,3 +0,0 @@
-var x = prompt( " Enter Distance in kilometers! ");
-var y = x*1000;
-alert(" The distance in meters is "+y+" m.");
